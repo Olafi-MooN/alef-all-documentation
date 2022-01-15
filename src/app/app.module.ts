2daeb26b09e2adf5be8cc8f786925017eb85ac1c
Olafi-MooN <alefmastertutor@gmail.com>
2022-01-15 17:25:13 -0300

    criacao do menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MakeTheJobModule } from '@olafi-moon/make-the-job';
 
 import { AppComponent } from './app.component';
 import { DarkModeComponent } from './shared/components/dark-mode/dark-mode.component';
 import { LayoutComponent } from './shared/components/layout/layout.component';
+import { SidebarComponent } from './shared/components/layout/sidebar/sidebar.component';
+import { LiListComponent } from './shared/components/layout/sidebar/li-list/li-list.component';
+import { NavbarComponent } from './shared/components/layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DarkModeComponent,
-    LayoutComponent
+    LayoutComponent,
+    SidebarComponent,
+    LiListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     MakeTheJobModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ src/app/shared/components/layout/sidebar/li-list/li-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'alef-li-list',
+  templateUrl: './li-list.component.html',
+  styleUrls: ['./li-list.component.css']
+})
+export class LiListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
