bf16d1020289db850b967741fe1998142d353d0a
Olafi-MooN <alefmastertutor@gmail.com>
2022-02-13 22:39:57 -0300

    deploy application

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAL57wlX0_m5eaaA-NueSCAPZvguCky88k',
+    authDomain: 'alef-all-documentation.firebaseapp.com',
+    projectId: 'alef-all-documentation',
+    storageBucket: 'alef-all-documentation.appspot.com',
+    messagingSenderId: '533302456341',
+    appId: '1:533302456341:web:c53529ba91ac81973e9e1c',
+  }
 };
